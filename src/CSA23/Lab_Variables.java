5cb2d94a7b33b489093af15b07f236e95701c8f1
AzmayenMurshid <azmayen.murshid@gmail.com>
2022-09-19 08:43:54 -0500

    Lab: Variables

---
+++ src/CSA23/Lab_Variables.java
@@ -0,0 +1,20 @@
+package CSA23;
+
+public class Lab_Variables {
+    public static void main(String[] args) {
+        byte varByte = 123;
+        short varShort = -22344;
+        int varInt = 1232;
+        long varLong = 1243;
+        float varFloat = 12.23f;
+        double varDouble = 123.2321;
+        char varChar = 'A';
+        boolean varBoolean = true;
+        String varString = "Azmayen Murshid";
+
+        System.out.println("Byte Variable: " + varByte + "\nShort Variable: " + varShort +"\nInteger Variable: "
+                + varInt + "\nLong Variable: " + varLong + "\nFloat Variable: " + varFloat + "\nDouble Variable: "
+                + varDouble + "\nChar Variable: " + varChar + "\nBoolean Variable: " + varBoolean
+                + "\nString Variable: " + varString);
+    }
+}
